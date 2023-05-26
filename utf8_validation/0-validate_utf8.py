5e58e3d5a5d2cf5fde683b2fb68c200c28181b03
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-26 17:18:44 +0200

    Task Utf 8validation

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """ method that determines if a given data set represents a valid UTF-8 encoding"""
+
+    numberofbytes = 0
+    b1 = 1 << 7
+    b2 = 1 << 6
+
+    for d in data:
+        m = 1 << 7
+        if numberofbytes == 0:
+            while m & d:
+                numberofbytes += 1
+                m = m >> 1
+            if numberofbytes == 0:
+                continue
+            if numberofbytes == 1 or numberofbytes > 4:
+                return False
+        else:
+            if not (d & b1 and not (d & b2)):
+                return False
+        numberofbytes -= 1
+    return numberofbytes == 0
