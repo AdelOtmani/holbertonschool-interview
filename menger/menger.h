04e20e445e1a83c2da600d77ba11b3ba11a383a6
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-06-08 22:35:30 +0200

    menger Cube

---
+++ menger/menger.h
@@ -0,0 +1,12 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+void menger(int level);
+bool printcube(int i, int j);
+
+#endif
