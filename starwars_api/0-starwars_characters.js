bbdac8bd4218e84d00714d09cbec1ef699b149ed
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-07-14 18:19:21 +0200

    Task

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,23 +2,22 @@
 
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-
 request(url, async function (error, response, body) {
-    if (error) {
-        console.log(error);
-    }else{
+  if (error) {
+    console.log(error);
+  } else {
     const characters =JSON.parse(body).characters;
     for (const actor in characters) {
-        const res = await new Promise ((resolve, reject) => {
-            request(characters[actor], (error, res, html) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve (JSON.parse(html).name)
-                }
-            });
+      const res = await new Promise ((resolve, reject) => {
+        request(characters[actor], (error, res, html) => {
+          if (error) {
+            reject(error);
+            } else {
+              resolve (JSON.parse(html).name)
+            }
+          });
         });
-        console.log(res);
-        }
+      console.log(res);
     }
+  }
 });
