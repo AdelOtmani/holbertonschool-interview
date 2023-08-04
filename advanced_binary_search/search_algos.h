d6ed2127207a295707f1ac33862361d1d0043902
AdelOtmani <5023@holbertonschool.com>
2023-08-04 17:23:30 +0200

    Task advanced binary

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+#include <stdio.h>
+int advanced_binary(int *array, size_t size, int value);
+int index_array(int *array, size_t size, int value, int l, int h);
+
+
+
+#endif
