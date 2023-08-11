9fcd931a26dddcd4145b9f91ccf8ba4e0e92d808
AdelOtmani <5023@holbertonschool.com>
2023-08-11 18:56:45 +0200

    Task Count it

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-Write a recursive function that queries the Reddit API, parses the title of all hot articles,
+Write a recursive function that queries the Reddit API,
+parses the title of all hot articles,
 and prints a sorted count of given keywords
-(case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not).
+(case-insensitive, delimited by spaces.
+Javascript should count as javascript, but java should not).
 """
 from requests import request
+
 
 def dict(word_list):
     """
