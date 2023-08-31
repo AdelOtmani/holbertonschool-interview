5e8d4f37f4a26e3766293fb3a0a438a9dde8177e
AdelOtmani <5023@holbertonschool.com>
2023-08-31 19:24:52 +0200

    Task rotate 2d matrix

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,15 +3,16 @@
 Rotating a matrix2D
 """
 
-def rotate_2d_matrix(matrice):
-   """
-   2D matrix, rotate it 90 degrees clockwise.
-   """
-   for i in range(0, len(matrice[0])):
-    for j in range (i, len(matrice[0])):
-      tmp = matrice[i][j]
-      matrice[i][j] = matrice[j][i]
-      matrice[j][i] = tmp
 
-   for j in range(len(matrice[0])):
-    matrice[j].reverse()
+def rotate_2d_matrix(matrice):
+    """
+   2D matrix, rotate it 90 degrees clockwise.
+    """
+    for i in range(0, len(matrice[0])):
+        for j in range (i, len(matrice[0])):
+            tmp = matrice[i][j]
+            matrice[i][j] = matrice[j][i]
+            matrice[j][i] = tmp
+
+    for j in range(len(matrice[0])):
+        matrice[j].reverse()
