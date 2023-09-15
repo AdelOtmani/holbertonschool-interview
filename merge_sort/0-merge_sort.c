cea911df38ea99397b325060936da091915225d1
AdelOtmani <5023@holbertonschool.com>
2023-09-15 22:41:46 +0200

    betty code solved

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -3,13 +3,13 @@
 
 /**
  * merge_function - merge sorted array
- * @array: input array
- * @output: array
+ * @array: input pointer array
+ * @z: output pointer array
  * @left: leftpoint index array
  * @mid: midpoint array
  * @right: right endpoint array
  */
-void merge_function(int *array, int *output, size_t left, size_t mid, size_t right)
+void merge_function(int *array, int *z, size_t left, size_t mid, size_t right)
 {
 	size_t i, j, m, n = right - left;
 
@@ -24,67 +24,67 @@ void merge_function(int *array, int *output, size_t left, size_t mid, size_t rig
 	{
 		if (i < mid && (j >= right || array[i] <= array[j]))
 		{
-			output[m + left] = array[i];
+			z[m + left] = array[i];
 			i++;
 		}
 		else
 		{
-			output[m + left] = array[j];
+			z[m + left] = array[j];
 			j++;
 		}
 	}
 	printf("[Done]: ");
-	print_array((const int *) output + left, n);
+	print_array((const int *) z + left, n);
 }
 
 /**
  * merge_split - split and merge
- * @output: array
- * @array: array
+ * @z: output pointer array
+ * @array: pointer array
  * @left: left index of array
  * @right: right index of array
  */
-void merge_split(int *output, int *array, size_t left, size_t right)
+void merge_split(int *z, int *array, size_t left, size_t right)
 {
 	size_t mid;
 
 	if (right - left <= 1)
 		return;
 	mid = (right + left) / 2;
-	merge_split(array, output, left, mid);
-	merge_split(array, output, mid, right);
-	merge_function(output, array, left, mid, right);
+	merge_split(array, z, left, mid);
+	merge_split(array, z, mid, right);
+	merge_function(z, array, left, mid, right);
 }
 
 /**
  * copy - copy array
- * @array: input array
- * @output: array to copy from
+ * @array: input pointer array
+ * @z: output pointer array
  * @size: array
  */
-void copy(int *array, int *output, size_t size)
+void copy(int *array, int *z, size_t size)
 {
 	size_t i;
 
 	for (i = 0; i < size; i++)
-		array[i] = output[i];
+		array[i] = z[i];
 }
 
 /**
  * merge_sort - merge sort algorithm
- * @array: array
+ * @array: pointer array
  * @size: array
  */
 void merge_sort(int *array, size_t size)
 {
-	int *output;
+	int *z;
 
 	if (array == NULL || size <= 1)
 		return;
-	output = malloc(sizeof(array[0]) * size);
-	if (!output)
+	z = malloc(sizeof(array[0]) * size);
+	if (!z)
 		return;
-	copy(output, array, size);
-	merge_split(output, array, 0, size);
-	free(output);
+	copy(z, array, size);
+	merge_split(z, array, 0, size);
+	free(z);
 }
