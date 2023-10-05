3b1856e46b23e04dcef50c2608ea185a663c80d5
AdelOtmani <5023@holbertonschool.com>
2023-10-05 12:23:03 +0200

    betty correction

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -41,7 +41,7 @@ List *add_node_end(List **list, char *str)
 }
 
 /**
- * add_node_begin - Add a new node to the begin of a double circular linked list
+ * add_node_begin-Add a new node to the begin of a double circular linked list
  * @list: the list to modify
  * @str: the string to copy into the new node
  * Return: Address of the new node, or NULL on failure
