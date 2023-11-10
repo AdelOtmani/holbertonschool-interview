fe07bb35f6caa0439cca44feb65409778d187fb0
AdelOtmani <5023@holbertonschool.com>
2023-11-10 23:15:28 +0100

    Pascal's Triangle fix

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """that returns a list of lists of integers
+        representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    pascal = [[1]]
+    for _ in range(n - 1):
+       pascal += [[1] + [pascal[-1][i] + pascal[-1][i + 1]
+                   for i in range(len(pascal[-1]) - 1)] + [1]]
+    return pascal
