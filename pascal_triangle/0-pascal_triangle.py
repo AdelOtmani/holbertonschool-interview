1c371a6b83b7b410b4d24887dc207f16fcecd489
AdelOtmani <5023@holbertonschool.com>
2023-11-10 23:17:23 +0100

    pep fix

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -9,6 +9,6 @@ def pascal_triangle(n):
         return []
     pascal = [[1]]
     for _ in range(n - 1):
-       pascal += [[1] + [pascal[-1][i] + pascal[-1][i + 1]
-                   for i in range(len(pascal[-1]) - 1)] + [1]]
+        pascal += [[1] + [pascal[-1][i] + pascal[-1][i + 1]
+                    for i in range(len(pascal[-1]) - 1)] + [1]]
     return pascal
