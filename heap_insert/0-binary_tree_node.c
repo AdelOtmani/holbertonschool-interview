3786069f17ff140de75c21e0b6e0e4ef7dc8bfaf
AdelOtmani <5023@holbertonschool.com>
2024-05-24 17:02:34 +0200

    Task 0 Done (Work on local) with Readme

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include<stdio.h>
+/**
+ * binary_tree_node - Adds a new node to a binary tree
+ * @parent: parent of the created node
+ * @value: the data of the node
+ * Return: pointer to the created node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *node;
+
+	/* Save space in memory */
+	node = malloc (sizeof(binary_tree_t));
+	if(!node)
+		return (0);
+
+	/* Create new node */
+	node->parent = parent;
+	node->n = value;
+	return (node);
+}
---
+++ heap_insert/README.md
@@ -0,0 +1,7 @@
+# directory insert_in_sorted_linked_list by A.OTMANI
+## Write a function that creates a binary tree node
+        Prototype: binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+        Return: a pointer to the new node, or NULL on failure
+## Write a function that inserts a value into a Max Binary Heap
+        Prototype: heap_t *heap_insert(heap_t **root, int value);
+        Return: a pointer to the inserted node, or NULL on failure
