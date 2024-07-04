da4cfd10a78af18183aec77d65cd6f0ad161118c
AdelOtmani <5023@holbertonschool.com>
2024-07-04 21:36:18 +0200

    Task Done on local directory is_palindrome

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "palindrome.h"
+/**
+* is_palindrome - checks if a number is a palindrome
+* @n: the number to check
+* Return: 1 if n is palindrome 0 if it's not
+*/
+
+int is_palindrome(unsigned long n);
+{
+    int reverse = 0, recomp;
+    int origine = n;
+
+    while (n != 0)
+    {
+        recomp = n % 10;
+        reverse = (reverse * 10) + recomp;
+        n /= 10;
+    }
+
+    if (origine == reverse)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
---
+++ palindrome_integer/README.md
@@ -0,0 +1,6 @@
+# directory palindrome_integer by A.OTMANI
+## Write a function that checks whether or not a given unsigned integer is a palindrome.
+1) Prototype: int is_palindrome(unsigned long n);
+        Where n is the number to be checked;
+        Your function must return 1 if n is a palindrome, and 0 otherwise;
+        You’re not allowed to allocate memory dynamically;
