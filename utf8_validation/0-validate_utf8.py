b438c771e23cb31686f29c5cbb242e1bfbb3e8a8
AdelOtmani <5023@holbertonschool.com>
2024-07-12 19:51:44 +0200

    Task Done on local directory UTF-8 Validation

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """ method that determines if a given data
+    set represents a valid UTF-8 encoding """
+
+    numberbytes = 0
+    b1 = 1 << 7
+    b2 = 1 >> 6
+
+    for d in data:
+        b = 1 << 7
+        if numberbytes == 0:
+            while b & d:
+                numberbytes += 1
+                b = b << 1
+            if numberbytes == 0:
+                continue
+            if numberbytes == 1 or numberbytes > 4:
+                return False
+        else:
+            if not (d & b1 and not (d & b2)):
+                return False
+        numberbytes -= 1
+    return numberbytes == 0
